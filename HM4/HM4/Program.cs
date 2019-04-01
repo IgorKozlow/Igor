20628bb63c61791e814e6fdc07df30c86c3f55ad
Ihar <IharKozlow@gmail.com>
2019-04-01 19:45:00 +0300

    Task from presentation

---
+++ HM4/HM4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace case_2
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int z = Convert.ToInt32(Console.ReadLine());
+        
+                Console.WriteLine("число z = " + z);
+
+            do
+            {
+                if (z < 11) {++z;
+                Console.WriteLine(z);
+            }
+                else Console.WriteLine("chislo больше 11 ");
+
+            } while (z < 11);
+
+
+
+
+            Console.WriteLine("число z = {0}",z);
+
+
+
+
+
+            Console.ReadKey();
+
+            
+
+
+        }
+    }
+}
---
+++ Presentation_Task/Presentation_Task/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Presentation_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
+        }
+
+        static void Test1()
+
+        {
+            int[] array = new int[] { };
+
+            Console.WriteLine(array);
+            Console.ReadKey();
+        }
+        static void Test2()
+        {
+            object[] array = {32,'A', "Hello" };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            array[0] = Convert.ToInt32(array[0]) + 10;
+            array[2] = Convert.ToString(array[2]) + ", guys";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+        static void Test3()
+        {
+            Random random = new Random();
+
+            int[] array = new int[13];
+
+            int a = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+                Console.WriteLine(array[i]);
+
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[a] > array[i])
+                {
+                    Console.WriteLine("  x = {0}   y = {1}", array[a], array[i]);
+                    Console.ReadKey();                  
+                }
+                else
+                {
+                    a = i;
+                    Console.WriteLine("  x = {0}   y = {1}", array[a], array[i]);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
